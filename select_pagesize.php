c73301eb48930d74cc1517be10c0dc773fb4c58a
tbsmcd <tbsmcd@gmail.com>
2019-07-23 18:21:51 +0900

    remove js, just run only php

--- select_pagesize.php
+++ select_pagesize.php
@@ -15,8 +15,8 @@ class select_pagesize extends rcube_plugin
         $pagesize_options = $this->config->get('pagesize_options');
         $this->output->set_env('pagesize_options', $pagesize_options);
 
+        $this->add_hook('preferences_list', [$this, 'preferences_list']);
         $this->add_hook('preferences_save', [$this, 'preferences_save']);
-        $this->include_script('js/select_pagesize.js');
     }
 
     function set_config($config)
@@ -27,6 +27,30 @@ class select_pagesize extends rcube_plugin
     function set_output($output)
     {
         $this->output = $output;
+    }
+
+    function preferences_list($args)
+    {
+        if ($args['section'] === 'mailbox') {
+//            if
+
+            $rc = rcmail::get_instance();
+            $prefs = $rc->user->get_prefs();
+            $current = isset($prefs['mail_pagesize']) ? $prefs['mail_pagesize'] : null;
+
+            $select = new html_select(['name' => '_mail_pagesize', 'id' => 'rcmfd_mail_pagesize']);
+
+            foreach ($this->config->get('pagesize_options') as $size) {
+                $select->add($size, $size);
+            }
+
+            $args['blocks']['main']['options']['pagesize'] = [
+                'title' => $this->gettext('pagesize'),
+                'content' => $select->show($current)
+            ];
+        }
+
+        return $args;
     }
 
     function preferences_save($args)
